1e59e2dec2ae7ed1cc653527ba7e7b2c5e626350
Raphael Hehl <support@technohehl.de>
2018-01-02 10:53:31 +0100

    combined form.php with index.html

--- form.php
+++ form.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: praktikant
- * Date: 02.01.2018
- * Time: 09:58
- */
+if(isset($_POST["submit"])){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Praktikum bei Newsfactory</title>
+</head>
+<body>
+<h1>Praktikum bei Newsfactory</h1>
+
+<h2>Datum</h2>
+<h2>Formular</h2>
+<form action="" method="post">
+    Name: <br>
+    <input type="text" name="name"><br>
+    Email Adresse: <br>
+    <input type="email" name="email"><br>
+    Nachricht: <br>
+    <textarea name="message"></textarea><br>
+    <input type="submit" name="submit" value="Senden">
+</form>
+<p><?php echo $name; ?></p>
+</body>
+</html>
