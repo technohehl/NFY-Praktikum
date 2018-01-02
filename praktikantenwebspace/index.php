9fca7724fe4235f1075d4d14be8c2cc2f6ee75a6
Raphael Hehl <support@technohehl.de>
2018-01-02 11:43:03 +0100

    cleaning code

--- praktikantenwebspace/index.php
+++ praktikantenwebspace/index.php
@@ -14,21 +14,25 @@ if(isset($_POST["submit"])){
     <title>Praktikum bei Newsfactory</title>
 </head>
 <body>
-<h1>Praktikum bei Newsfactory</h1>
+<div>
+    <div>
+        <h1>Praktikum bei Newsfactory</h1>
+    </div>
 
-<h2>Datum</h2>
-Das Datum ist: <?php echo date("d F Y"); ?>
-<h2>Formular</h2>
-<h3>Eingabe</h3>
-<form action="" method="post">
-    Name: <br><input type="text" name="name"><br>
-    Email Adresse: <br><input type="email" name="email"><br>
-    Nachricht: <br><textarea name="message"></textarea><br>
-    <input type="submit" name="submit" value="Senden"><br>
-</form>
-<h3>Ausgabe</h3>
-<p>Name: <?php echo $name; ?></p>
-<p>Email: <?php echo $email; ?></p>
-<p>Nachricht: <?php echo $message; ?></p>
+    <h2>Datum</h2>
+        Das Datum ist: <?php echo date("d F Y"); ?>
+    <h2>Formular</h2>
+        <h3>Eingabe</h3>
+        <form action="" method="post">
+            Name: <br><input type="text" name="name"><br>
+            mail Adresse: <br><input type="email" name="email"><br>
+            Nachricht: <br><textarea name="message"></textarea><br>
+            <input type="submit" name="submit" value="Senden"><br>
+        </form>
+    <h3>Ausgabe</h3>
+    <p>Name: <?php echo $name; ?></p>
+    <p>Email: <?php echo $email; ?></p>
+    <p>Nachricht: <?php echo $message; ?></p>
+</div>
 </body>
 </html>
